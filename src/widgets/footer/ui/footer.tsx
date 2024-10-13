ea5960f9823204a44619d82749912ddd7bcac2de
Maksim <dymufa@gmail.com>
2024-10-13 18:28:21 +0300

    feat: create footer

--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -1,5 +1,17 @@
 import styles from "./footer.module.scss";
 
 export const Footer = () => {
-	return <div className={styles.footer}>Footer</div>;
+	return (
+		<div className={styles.footer}>
+			<img alt="Site logo" className={styles.footer__image} src="/logo.png" />
+			<p className={styles.footer__terms}>
+				© 2024 CryptoDrop — ловите криптовалюту с неба{" "}
+			</p>
+			<div className={styles.footer__text}>
+				<p>На нашем сайте вы можете открыть</p>
+				<p>различные криптовалютные кейсы</p>
+				<p>с высокой окупаемостью.</p>
+			</div>
+		</div>
+	);
 };
