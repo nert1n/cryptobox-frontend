f6e1822dc3e199e3d7079226364de4878ce808a9
nert1n <dymufa@gmail.com>
2024-11-08 17:07:23 +0200

    feat: update payment services

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
 		proxy: {
 			"/api": {
 				target:
-					"https://acf4-2a02-8071-6282-a220-c56-ddb1-d70e-94f7.ngrok-free.app",
+					"https://c7b4-2a02-8071-6282-a220-c56-ddb1-d70e-94f7.ngrok-free.app",
 				changeOrigin: true,
 				secure: true,
 				cookieDomainRewrite: "localhost",
