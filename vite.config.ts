9122e20d5a8639832783d44be970fa8e009fd1e3
nert1n <dymufa@gmail.com>
2024-11-27 21:43:00 +0200

    fe

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
 		proxy: {
 			"/api": {
 				target:
-					"https://6eee-2a02-8071-6282-a220-4569-d771-9d4f-5766.ngrok-free.app",
+					"https://62ae-2a02-8071-6282-a220-58c3-5ee6-9af6-b3b8.ngrok-free.app",
 				changeOrigin: true,
 				secure: true,
 				cookieDomainRewrite: "localhost",
